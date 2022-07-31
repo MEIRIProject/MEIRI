8f03f42223c6a534e8e48702543008a49a64a71b
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-31 23:42:55 +0900

    COMMIT

--- src/com/meiri/jsp/cart/controller/CartInsert.java
+++ src/com/meiri/jsp/cart/controller/CartInsert.java
@@ -15,8 +15,8 @@ import com.meiri.jsp.cart.model.vo.Cart;
  */
 @WebServlet("/cartInsert.ca")
 public class CartInsert extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,37 +25,37 @@ public class CartInsert extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.setContentType("text/html; charset=UTF-8");
-		
-		String userid = request.getParameter("userid");
-		int pcode = Integer.parseInt(request.getParameter("pcode"));
-		int fcode = Integer.parseInt(request.getParameter("fcode"));
-		int quantity = Integer.parseInt(request.getParameter("Quantity"));
-		
-		Cart ca = new Cart();
-		
-		ca.setUserid(userid);
-		ca.setFcode(fcode);
-		ca.setPcode(pcode);
-		ca.setQuantity(quantity);
-		
-		String yes = new CartService().InsertCart(ca);
-		
-		response.getWriter().print(yes);
-		
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        response.setContentType("text/html; charset=UTF-8");
+
+        String userid = request.getParameter("userid");
+        int pcode = Integer.parseInt(request.getParameter("pcode"));
+        int fcode = Integer.parseInt(request.getParameter("fcode"));
+        int quantity = Integer.parseInt(request.getParameter("Quantity"));
+
+        Cart ca = new Cart();
+
+        ca.setUserid(userid);
+        ca.setFcode(fcode);
+        ca.setPcode(pcode);
+        ca.setQuantity(quantity);
+
+        String yes = new CartService().InsertCart(ca);
+
+        response.getWriter().print(yes);
+
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
 
 }
