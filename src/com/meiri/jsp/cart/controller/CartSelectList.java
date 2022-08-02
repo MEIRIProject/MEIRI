6f16b432ba47847d5d8dbca851fa710d44bbafb9
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-08-02 16:57:38 +0900

    COMMIT

--- src/com/meiri/jsp/cart/controller/CartSelectList.java
+++ src/com/meiri/jsp/cart/controller/CartSelectList.java
@@ -17,8 +17,8 @@ import com.meiri.jsp.cart.model.vo.CartView;
  */
 @WebServlet("/selectCart.ca")
 public class CartSelectList extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,39 +27,39 @@ public class CartSelectList extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String userid = request.getParameter("userid");
-		
-		System.out.println("userid = " + userid);
-		
-		ArrayList<CartView> cav = new CartService().selectCartList(userid);
-		
-		String page = "";
-		
-		if(cav != null) {
-			request.setAttribute("cartlist", cav);
-			
-			page = "views/basket/basketShopping.jsp";
-		} else {
-			request.setAttribute("exception", new Exception("게시글 상세 조회 실패"));
-			request.setAttribute("error-msg", "게시글 상세 조회 실패!!");
-			
-			page = "views/common/errorPage.jsp";
-		}
-		
-		request.getRequestDispatcher(page).forward(request, response);
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        String userid = request.getParameter("userid");
+
+        System.out.println("userid = " + userid);
+
+        ArrayList<CartView> cav = new CartService().selectCartList(userid);
+
+        String page = "";
+
+        if (cav != null) {
+            request.setAttribute("cartlist", cav);
+
+            page = "views/basket/basketShopping.jsp";
+        } else {
+            request.setAttribute("exception", new Exception("게시글 상세 조회 실패"));
+            request.setAttribute("error-msg", "게시글 상세 조회 실패!!");
+
+            page = "views/common/errorPage.jsp";
+        }
+
+        request.getRequestDispatcher(page).forward(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
 
 }
