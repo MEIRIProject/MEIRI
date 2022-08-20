39dd6326c3f78965e8e8c016f0068509eaa024c9
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-08-20 21:26:56 +0900

    COMMIT

--- src/com/meiri/jsp/common/EncodingFilter.java
+++ src/com/meiri/jsp/common/EncodingFilter.java
@@ -13,42 +13,42 @@ import javax.servlet.annotation.WebFilter;
  * @WebFilter 는 특정 url이나 서블릿을 거쳐갈 때
  * 해당하는 필터를 동작 시켜라 라는 뜻.
  * ex)  /login.do : 로그인 서블릿을 실행 때만 수행하는 필터
- * ex2) /* : 모든 서블릿을 실행 할 때 수행하는 필터 
+ * ex2) /* : 모든 서블릿을 실행 할 때 수행하는 필터
  */
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
 
     /**
-     * Default constructor. 
+     * Default constructor.
      */
     public EncodingFilter() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// 서블릿 수행 전
-		request.setCharacterEncoding("UTF-8");
-		
-		chain.doFilter(request, response); // 필터 수행 후 서블릿 연동
-		
-		// 서블릿 수행 후
-	}
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 서블릿 수행 전
+        request.setCharacterEncoding("UTF-8");
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
+        chain.doFilter(request, response); // 필터 수행 후 서블릿 연동
+
+        // 서블릿 수행 후
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
 
 }
